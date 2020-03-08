175c1874d615fee0767445a6c9bf63bbd791ddaf
harus666 <harnik.rafal@gmail.com>
2020-03-08 10:16:38 +0100

    Initial commit

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,15 @@
+public class TicTacToe {
+
+        public static void main(String[] args) {
+
+        }
+     class board {
+
+     int[][] board = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+    }
+}
